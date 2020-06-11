8b987d5c512e113a5cf9e80671b1c7d0211bf1f2
scoopgracie <scoopgracie@gmail.com>
2020-06-11 06:56:02 -0700

    Create db.py
    Create some dummy functions for the db

---
+++ recodein/db.py
@@ -0,0 +1,11 @@
+'''
+db.py - database handler for recodein
+currently provides dummy functions
+'''
+
+
+def add_student(username, name, email, password):
+    print(f'''Added student with username '{username}', name '{name}', email '{email}', and password '{password}'''')
+
+def add_mentor(username, name, email, password):
+    print(f'''Added mentor with username '{username}', name '{name}', email '{email}', and password '{password}'''')
